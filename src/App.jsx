42d378f9e6835333adc902f0c76e0f4dda70a922
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2023-04-08 16:19:08 +0500

    Adds Redux Toolkit Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-import './App.css';
+import { useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api';
 
 const App = () => {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular').then((res) => {
+      console.log(res);
+    });
+  };
   return <div className="App">Hey World</div>;
 };
 
