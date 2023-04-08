44512180b537dd137840d8228fc231fc57e6cc16
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2023-04-08 16:36:26 +0500

    Adds Api call and save in Redux Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useEffect } from 'react';
 import { fetchDataFromApi } from './utils/api';
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
   useEffect(() => {
     apiTesting();
   }, []);
   const apiTesting = () => {
     fetchDataFromApi('/movie/popular').then((res) => {
       console.log(res);
+      dispatch(getApiConfiguration(res));
     });
   };
-  return <div className="App">Hey World</div>;
+  return <div className="App">{url?.total_pages}</div>;
 };
 
 export default App;
